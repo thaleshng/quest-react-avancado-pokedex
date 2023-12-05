6cd39042cdffa9f67beb31af84c823a264448f01
Thales Gomes <thaleshng@gmail.com>
2023-12-04 22:20:04 -0300

    mudando icon dos temas e salvando valor do checked

--- src/components/theme-toggler-button/theme-toggler-button.jsx
+++ src/components/theme-toggler-button/theme-toggler-button.jsx
@@ -1,15 +1,34 @@
-import { useContext } from "react"
+import { useContext, useState, useEffect } from "react"
 import { ThemeContext, themes } from "../../contexts/theme-context"
-// import { ThemeButton } from "../button/button"
 import { ThemeInput } from "../button/input"
 
 export const ThemeTogglerButton = () => {
-    
-    const { theme, setTheme } = useContext(ThemeContext)
+    const { theme, setTheme } = useContext(ThemeContext);
+    const [isChecked, setIsChecked] = useState(() => {
+        const storedTheme = localStorage.getItem("theme");
+        return storedTheme === JSON.stringify(themes.moon) || theme === themes.moon;
+    });
+
+    useEffect(() => {
+        const storedTheme = localStorage.getItem("theme");
+        setIsChecked(storedTheme === JSON.stringify(themes.moon) || theme === themes.moon);
+    }, [theme]);
+
+    const toggleTheme = () => {
+        const newTheme = theme === themes.sun ? themes.moon : themes.sun;
+        setTheme(newTheme);
+        localStorage.setItem("theme", JSON.stringify(newTheme));
+    };
     
     return (
         <>
-            <ThemeInput type="checkbox" role="switch" checked={theme === themes.moon} theme={theme} onClick={() => setTheme(theme === themes.sun ? themes.moon : themes.sun)} />
+            <ThemeInput 
+                type="checkbox" 
+                role="switch" 
+                checked={isChecked} 
+                theme={theme} 
+                onClick={toggleTheme} 
+            />
         </>
     )
 }
